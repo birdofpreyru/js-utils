d8fdd6febcc6fbb33b484c17965bddb97c1c0ffd
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-02-20 14:44:29 +0100

    [#6] Improves `withRetries()` typing

--- src/withRetries.ts
+++ src/withRetries.ts
@@ -11,16 +11,16 @@ import { timer } from './time';
  * @returns Resolves to the result of the successful `action` execution;
  *  or rejects with the error from the last faileda attempt.
  */
-export default async function withRetries(
-  action: () => unknown,
+export default async function withRetries<T>(
+  action: () => T,
   maxRetries = 3,
   interval = 300,
-) {
+): Promise<Awaited<T>> {
   /* eslint-disable no-await-in-loop */
   for (let n = 1; ; ++n) {
     try {
       const res = action();
-      return res instanceof Promise ? await res : res;
+      return res instanceof Promise ? await res : (res as Awaited<T>);
     } catch (error) {
       if (n < maxRetries) await timer(interval);
       else throw error;
