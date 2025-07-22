4bf30d49b92c589c002ca9ad9d8d7ae5d19bd7ce
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-07-22 17:48:01 +0200

    v0.1.2: Fixes CJS export

---
+++ cjs.js
@@ -0,0 +1,10 @@
+/* global module, require */
+
+// eslint-disable-next-line import/no-commonjs
+const jsUtils = require('./build/common/index.js').default;
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = jsUtils;
+
+// eslint-disable-next-line import/no-commonjs
+module.exports.default = jsUtils;
