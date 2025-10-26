405ebdf5213a7312accb92a50883c4ba3b91697b
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-10-26 23:46:11 +0100

    v0.1.4: Update of dependencies + Fixes of module build

--- babel.module.config.js
+++ babel.module.config.js
@@ -1,6 +1,9 @@
 // Babel is used for Jest testing.
 
 export default {
+  plugins: [
+    'babel-plugin-add-import-extension',
+  ],
   presets: [
     ['./config/babel/preset', {
       modules: false,
